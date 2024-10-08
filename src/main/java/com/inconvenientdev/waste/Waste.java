5cba7698a370aa604cebcfaa2b1ca3094891d138
Dustin Heisey <dustin@heisey.email>
2024-10-08 16:14:10 +0200

    feat: add first item

--- src/main/java/com/inconvenientdev/waste/Waste.java
+++ src/main/java/com/inconvenientdev/waste/Waste.java
@@ -1,10 +1,12 @@
 package com.inconvenientdev.waste;
 
+import com.inconvenientdev.waste.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -29,8 +31,11 @@ public class Waste
 
     public Waste()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/inconvenientdev/waste/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.inconvenientdev.waste.item;
+
+import com.inconvenientdev.waste.Waste;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Waste.MOD_ID);
+
+    public static final RegistryObject<Item> GARBAGE = ITEMS.register("garbage",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
