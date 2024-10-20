a49e5bc08e36f9f038923e2c7014319364de3776
Dustin Heisey <dustin@heisey.email>
2024-10-20 10:46:21 +0200

    feat: start custom tools

---
+++ src/main/java/com/inconvenientdev/waste/common/WasteTiers.java
@@ -0,0 +1,18 @@
+package com.inconvenientdev.waste.common;
+
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+
+public class WasteTiers {
+  public static final ForgeTier SCRAP_TIER =
+      new ForgeTier(
+          1,
+          300,
+          0.5F,
+          1,
+          4,
+          BlockTags.NEEDS_STONE_TOOL,
+          () -> Ingredient.of(WasteItems.SCRAP_METAL.get())
+      );
+}
